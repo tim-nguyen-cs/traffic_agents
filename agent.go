30a57e854271851e15dc3a14f474f8f1af936c11
tim-nguyen-cs <33771623+tim-nguyen-cs@users.noreply.github.com>
2017-11-19 19:43:32 -0800

    Add files via upload

---
+++ agent.go
@@ -0,0 +1,42 @@
+package main
+
+import "'strconv"
+
+type Agent interface {
+	Id() string
+	Start() string
+	Destination() string
+	LeaveTime() int
+}
+
+type agent struct {
+	ID int
+	
+	START_ID int 
+	DEST_ID int
+	DEPART_TIME int	
+
+	START_LOCATION string
+	DEST_LOCATION string	
+
+	ENVIR int
+	CST int
+	TIME int
+	
+}
+
+func (a agent) Id() string {
+	return strconv.Itoa(a.ID)
+}
+
+func (a agent) Start() string {
+	return a.START_LOCATION
+}
+
+func (a agent) Destination() string {
+	return a.DEST_LOCATION
+}
+
+func (a agent) LeaveTime() int {
+	return a.DEPART_TIME
+}
---
+++ edge.go
@@ -0,0 +1,42 @@
+package main
+
+type Edge interface {
+	Weight() float64
+	To() string
+	From() string
+	Time() int
+	AddAgent()
+	RemoveAgent()
+}
+
+type edge struct {
+	weight float64
+	from string
+	to string
+	time int
+	capacity int
+}
+
+func (e edge) Weight() float64 {
+	return e.weight
+}
+
+func (e edge) To() string {
+	return e.to
+}
+
+func (e edge) From() string {
+	return e.from
+}
+
+func (e edge) Time() int {
+	return e.time
+}
+
+func (e edge) AddAgent() {
+	e.capacity = e.capacity + 1
+}
+
+func (e edge) RemoveAgent() {
+	e.capacity = e.capacity - 1
+} 
